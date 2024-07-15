44b0d778119b1850f8a924ce2120cb2663de4b8b
zuii <167165607+zuii@users.noreply.github.com>
2024-07-15 10:44:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/de8de52639.js" crossorigin="anonymous"></script>
+    <title>Crystalofknowledge.com</title>
+    <link rel="icon" href="assets/logoUrl.png" type="image/png">
+  </head>
+  <body>
+    <!--Navbar-->
+   <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary shadow-sm justify-space-between">
+  <div class="container-fluid">
+    <img src="assets/logo.jpeg" alt="logo" width="50px">
+    <a href="sign/link_login.html" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#homeSection">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#aboutSection">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <section id="homeSection" class="p-5">
+      <div class="d-flex flex-wrap justify-content-center">
+        <div class="col mt-5">
+         <h2 class="fw-bold text-success"><span class="text-primary">Crystal</span>of knowledge</h2>
+          <p class="mb-4">"Temukan Dunia Pengetahuan di Ujung Jari Anda: <br> Perpustakaan Online <span class="fw-bold">Crystalofknowledge</span> Membawa Anda ke Dunia Buku Digital."</p>
+          <a class="btn btn-primary" href="sign/link_login.html">Get started</a>
+        </div>
+        <div class="col mt-3">
+          <img src="assets/logoDashboard-transformed.jpeg" width="450px">
+        </div>
+      </div>
+    </section>
+    
+    <section class="bg-body-secondary p-5" id="aboutSection">
+      <div class="row">
+          <div class="d-flex">
+            <h2 class="text-primary">About</h2>
+          </div>
+          <p>Selamat datang di Perpustakaan Digital XYZ, tempat di mana pengetahuan dan informasi tersedia di ujung jari Anda.
+             Kami menyediakan akses ke ribuan buku elektronik, jurnal, artikel, dan sumber daya digital lainnya untuk mendukung kebutuhan belajar dan penelitian Anda.
+            Perpustakaan digital merupakan inovasi dalam dunia perpustakaan yang menggabungkan teknologi informasi dengan tradisi penyimpanan dan penyebaran pengetahuan.
+            Konsep perpustakaan digital muncul sebagai respons terhadap perkembangan teknologi dan kebutuhan masyarakat akan akses informasi yang cepat, mudah, dan luas.</p>
+      </div>
+      <div class="d-flex justify-content-center">
+        <h3 class="text-secondary">Dikembangkan Oleh :</h3>
+      </div>
+
+    <div class="d-flex justify-content-center flex-wrap gap-4 mt-4">
+      <div class="card border-info mb-3" style="max-width: 22rem;">
+        <div class="card-header">Friends</div>
+        <div class="card-body">
+          <h5 class="card-title">Student At STMIK Royal Kisaran</h5>
+        </div>
+      </div>
+    </div>
+    </section>
+    
+    <footer id="footer" class="p-3 bg-dark">
+      <div class="row">
+        <div class="col">
+          <img src="assets/logo.jpeg" width="50px">
+        </div>
+      </div>
+      <div class="row p-3">
+        <div class="col mt-3">
+          <h3 class="text-light fs-5">Alamat</h3>
+          <p class="text-secondary fs-6">JL Sisingamangaraja Kisaran</p>
+        </div>
+        <hr class="text-light mt-3">
+        <div class="d-flex justify-content-center align-items-center mt-3">
+          <p class="text-light">Made with Friends ❤️ <a href="" class="text-decoration-none">Friends</a>  2024</p>
+        </div>
+      </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
